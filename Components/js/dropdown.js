1aa87749b78f15af4ddd8249aa04715d16998a20
Dung <dunggamo2000@gmail.com>
2021-07-16 22:48:19 +0700

    multi dropdown

--- Components/js/dropdown.js
+++ Components/js/dropdown.js
@@ -1,12 +1,28 @@
-var dropdown = document.querySelector(".dropdown");
+//DROPDOWN 1
+// var dropdown = document.querySelector(".dropdown");
 var dropdownList = document.querySelector(".dropdown-list");
-var iconDown = document.querySelector(".icon-down");
-var iconUp = document.querySelector(".icon-up");
 var dropdownValue = document.querySelector(".dropdown-value");
+
+
+//DROPDOWN 2
+// var dropdown2 = document.querySelector(".dropdown-2");
+var dropdownList2 = document.querySelector(".dropdown-list-2");
+var dropdownValue2 = document.querySelector(".dropdown-value-2");
+
+
+//DROPDOWN 3
+var dropdownList3 = document.querySelector(".dropdown-list-3");
+var dropdownValue3 = document.querySelector(".dropdown-value-3");
+
+// var iconDown = document.querySelector(".icon-down");
+// var iconUp = document.querySelector(".icon-up");
+
+console.log(dropdownList3);
+console.log(dropdownValue3);
+
 var state = false;
 
-var currVal = 4;
-
+var currVal = 0;
 
 var dropdownData = [
     "Tổng giám đốc",
@@ -17,33 +33,59 @@ var dropdownData = [
 ];
 
 
-render();
+
+var dropdownRestaurant = [
+    "Nhà hàng Trung Quốc",
+    "Nhà hàng Tàu Khựa",
+    "Nhà hàng Việt Nam",
+    "Nhà hàng Nhật bổn"
+];
+
+var dropdownGender = [
+    "Nam",
+    "Nữ",
+    "Không xác định"
+];
 
 
-function render() {
-    var dropdownListHTML = '';
-    dropdownValue.innerText = dropdownData[currVal];
 
-    for (var i = 0; i < dropdownData.length; i++) {
-        if (i != currVal) {
-            dropdownListHTML += `<li data-id=${i} class="dropdown-item"> ${dropdownData[i]} </li>`;
+
+function renderDropdown(dropdownValue, dropdownList, dropdownData) {
+
+    render();
+
+
+    function render() {
+        var dropdownListHTML = '';
+        dropdownValue.innerText = dropdownData[currVal];
+
+        for (var i = 0; i < dropdownData.length; i++) {
+            if (i == currVal) {
+                dropdownListHTML += `<li data-id=${i} class="dropdown-item dropdown-item--active"><i class="fas fa-check dropdown-item__icon"></i> ${dropdownData[i]} </li>`;
+            } else {
+                dropdownListHTML += `<li data-id=${i} class="dropdown-item"><i class="fas fa-check dropdown-item__icon"></i> ${dropdownData[i]} </li>`;
+            }
         }
-    }
 
-    dropdownList.innerHTML = dropdownListHTML;
+        dropdownList.innerHTML = dropdownListHTML;
 
-    var items = dropdownList.querySelectorAll('.dropdown-item');
+        var items = dropdownList.querySelectorAll('.dropdown-item');
 
-    items.forEach((item) => {
-        item.addEventListener('click', () => {
-            var dataId = item.getAttribute('data-id');
-            this.currVal = dataId;
-            this.render();
+        items.forEach((item) => {
+            item.addEventListener('click', () => {
+                var dataId = item.getAttribute('data-id');
+                this.currVal = dataId;
+                render();
+            });
         });
-    });
+    }
 }
 
+renderDropdown(dropdownValue, dropdownList, dropdownData);
 
+renderDropdown(dropdownValue2, dropdownList2, dropdownRestaurant);
+
+renderDropdown(dropdownValue3, dropdownList3, dropdownGender);
 
 
 // CÁCH 1
@@ -76,8 +118,37 @@ function render() {
 // });
 
 // CÁCH 3: 
-dropdown.addEventListener('click', function () {
-    dropdownList.classList.toggle('show');
-    iconUp.classList.toggle('show');
-    iconDown.classList.toggle('show');
+// dropdown.addEventListener('click', function () {
+//     dropdownList.classList.toggle('show');
+//     iconUp.classList.toggle('show');
+//     iconDown.classList.toggle('show');
+// });
+
+// dropdown2.addEventListener('click', function () {
+//     dropdownList2.classList.toggle('show');
+//     iconUp.classList.toggle('show');
+//     iconDown.classList.toggle('show');
+// });
+
+var dropdowns = document.querySelectorAll(".dropdown");
+
+// dropdowns[0].addEventListener('click', function () {
+//     dropdowns[0].querySelector('.dropdown-list').classList.toggle('show');
+//     iconUp.classList.toggle('show');
+//     iconDown.classList.toggle('show');
+// })
+
+// dropdowns[1].addEventListener('click', function () {
+//     dropdowns[1].querySelector('.dropdown-list').classList.toggle('show');
+//     iconUp.classList.toggle('show');
+//     iconDown.classList.toggle('show');
+// })
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', function () {
+        dropdown.querySelector('.dropdown-list').classList.toggle('show');
+        dropdown.querySelector('.icon-up').classList.toggle('show');
+        dropdown.querySelector('.icon-down').classList.toggle('show');
+    });
 });
+
